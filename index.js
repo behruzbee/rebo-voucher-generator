e8507364a5d090d203837c3bfbf2c0c079d06e5c
Behruz <behruz@bictory.io>
2024-06-26 16:44:49 +0500

    version 1.0.0

---
+++ index.js
@@ -0,0 +1,10 @@
+function generateUniqueId(length) {
+    const chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+      result += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return result;
+  }
+
+  document.getElementById('id_voucher').innerText = '#' + generateUniqueId(10);
